8fac3d54061ba6dfba4ab4c784c41bdcee3bfabf
Abgar Sim <abgar1223@gmail.com>
2018-07-04 17:58:52 -0500

    "Login controller returns players characters"

--- api/src/main/java/com/inther/controller/LoginController.java
+++ api/src/main/java/com/inther/controller/LoginController.java
@@ -1,14 +1,19 @@
 package com.inther.controller;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.TreeMap;
+
+import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
 
+import com.inther.entity.Character;
 import com.inther.entity.Player;
+import com.inther.repo.CharacterRepository;
 import com.inther.repo.PlayerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.support.PagedListHolder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,45 +26,58 @@ public class LoginController {
     @Autowired
     PlayerRepository playerRepository;
 
-    /*
-    * Login controller
-    * @param login - user login
-    * @param password - user password
-    * @return Json data
-    * */
+    @Autowired
+    CharacterRepository characterRepository;
 
-    @RequestMapping("/login")
-    public Map<String, String> login(@RequestParam(value = "login", defaultValue = "null") String login,
+    /*
+     * Login controller
+     * @param login - user login
+     * @param password - user password
+     * @return Json data
+     * */
+    @RequestMapping(value = "/login")
+    public Map<String, Object> login(@RequestParam(value = "login", defaultValue = "null") String login,
                                      @RequestParam(value = "password", defaultValue = "null") String password) {
-        LinkedHashMap<String, String> map = new LinkedHashMap<>();
+        LinkedHashMap<String, Object> map = new LinkedHashMap<>();
         if (!(login.equals("null")) && !(password.equals("null"))) {
             Player player = playerRepository.findByLogin(login);
             if (player == null) {
                 map.put("status", "failed");
-                map.put("message", "Player does not exist");
+                map.put("message", "no such player exist");
             } else {
                 if (player.getPassword().equals(password)) {
                     map.put("status", "success");
                     map.put("id", player.getID().toString());
                     map.put("points", player.getPoints().toString());
-                    //Put all Strength
+                    ArrayList<Character> characters = characterRepository.findByPlayerID(player.getID());
+                    Iterator<Character> iterator = characters.iterator();
+                    ArrayList<Map<String, Object>> result = new ArrayList<>();
+                    while (iterator.hasNext()) {
+                        Map<String, Object> characterMap = new TreeMap<>();
+                        Character character = iterator.next();
+                        characterMap.put("id", character.getID().toString());
+                        characterMap.put("nickname", character.getName());
+                        result.add(characterMap);
+                    }
+                    map.put("characters", result);
+
                 } else {
                     map.put("status", "failed");
                     map.put("message", "wrong password");
                 }
             }
         } else {
-            map.put("status", "failed");
-            map.put("message", "No login and/or password provided");
+            map.put("result", "failed");
+            map.put("message", "no login and/or password");
         }
         return map;
     }
 
     /*
-    * @param login - user login to register
-    * @param password - user password to register
-    * @return Json representation of result logs
-    * */
+     * @param login - user login to register
+     * @param password - user password to register
+     * @return Json representation of result logs
+     * */
     @RequestMapping("/register_player")
     public Map<String, String> newPlayer(@RequestParam(value = "login", defaultValue = "null") String login,
                                          @RequestParam(value = "password", defaultValue = "null") String password) {
@@ -77,20 +95,20 @@ public class LoginController {
                 return map;
             } catch (Exception e) {
                 map.put("status", "failed");
-                map.put("message", "An exception occurred");
+                map.put("message", "exception occurred");
                 return map;
             }
         } else {
             map.put("status", "failed");
-            map.put("message", "No login and/or password provided");
+            map.put("message", "no login and/or password");
             return map;
         }
     }
 
     /*
-    *
-    *
-    * */
+     *
+     *
+     * */
     @RequestMapping("/player_exist")
     public Map<String, String> playerExist(@RequestParam(name = "login", defaultValue = "kek") String login) {
         TreeMap<String, String> map = new TreeMap<>();
---
+++ api/src/main/java/com/inther/repo/CharacterRepository.java
@@ -0,0 +1,15 @@
+package com.inther.repo;
+
+
+import com.inther.entity.Character;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+
+@Repository
+public interface CharacterRepository extends JpaRepository<Character, Long>{
+    ArrayList<Character> findByPlayerID(Long playerId);
+}
