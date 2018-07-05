81e5271c4e5f7780de66cb0ed2dab7f5dbaf5aad
Ion Iascerinschi <iiasceri@student.42.fr>
2018-07-05 12:21:48 +0300

    Update ActivitiesRepository.java

--- api/src/main/java/com/inther/repo/ActivitiesRepository.java
+++ api/src/main/java/com/inther/repo/ActivitiesRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ActivitiesRepository extends JpaRepository<Character, Long>{
+public interface ActivitiesRepository extends JpaRepository<Activities, Long>{
 
 }
