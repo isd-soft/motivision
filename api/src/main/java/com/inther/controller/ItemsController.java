d7a08549237fe9960a91eae9d1387e6da6507b47
iiasceri <34134411+iiasceri@users.noreply.github.com>
2018-07-05 12:22:50 +0300

    "Added ItemsController"

---
+++ api/src/main/java/com/inther/controller/ItemsController.java
@@ -0,0 +1,45 @@
+package com.inther.controller;
+
+import com.inther.entity.Character;
+import com.inther.repo.CharacterRepository;
+import com.inther.repo.ItemsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+public class ItemsController {
+
+    @Autowired
+    ItemsRepository itemsRepository;
+
+    @Autowired
+    CharacterRepository characterRepository;
+
+
+    @RequestMapping(value = "/create_item", method = RequestMethod.POST)
+    public Map<String, Object> createItem(@RequestParam(value = "characterId") Long characterId,
+                                          @RequestParam(value = "typeId") Long typeId,
+                                          @RequestParam(value = "imageUrl") String imageUrl,
+                                          @RequestParam(value = "price") int price
+
+    ) {
+
+        LinkedHashMap<String, Object> map = new LinkedHashMap<>();
+        Optional<Character> optionlCharacter = characterRepository.findById(characterId);
+
+        if (!optionlCharacter.isPresent()) {
+            map.put("status", "failed");
+            map.put("message", "No such character exist with characterId");
+            return map;
+        }
+        return map;
+    }
+
+}
