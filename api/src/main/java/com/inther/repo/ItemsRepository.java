5e227ad24f2e90578ef3b79c71f0d552150d8e48
Ion Iascerinschi <iiasceri@student.42.fr>
2018-07-05 12:18:18 +0300

    Update ItemsRepository.java

--- api/src/main/java/com/inther/repo/ItemsRepository.java
+++ api/src/main/java/com/inther/repo/ItemsRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ItemsRepository extends JpaRepository<Character, Long>{
+public interface ItemsRepository extends JpaRepository<Items, Long>{
 
 }
