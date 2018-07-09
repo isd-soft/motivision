812691d6e1b254de7f9a4efe31044db6a72c2510
LC <liviu.cernei@gmail.com>
2018-07-09 12:00:24 +0300

    Responsive design for Character Selection Page

--- client/core/src/com/mygdx/game/screens/CharacterSelectScreen.java
+++ client/core/src/com/mygdx/game/screens/CharacterSelectScreen.java
@@ -2,6 +2,7 @@ package com.mygdx.game.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -19,6 +20,8 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.game.GameSets.GGame;
 import com.mygdx.game.requests.JsonHandler;
 import com.mygdx.game.requests.Player;
@@ -40,16 +43,18 @@ public class CharacterSelectScreen implements Screen {
     private Skin skin;
 
     ScrollPane scrollPane;
+    private Viewport viewport;
+    private Camera camera;
 
     public CharacterSelectScreen(GGame g) {
         parent = g;
-        stage = new Stage(new ScreenViewport());
-        skin = new Skin(Gdx.files.internal("skin/glassy-ui.json"));
+        stage = new Stage();
+        viewport = new StretchViewport(800, 480, stage.getCamera());
+        stage.setViewport(viewport);
     }
 
 
-
-    public Texture  splitImages() throws IOException {
+    public Texture splitImages() throws IOException {
         int width;
         int height;
         BufferedImage result = new BufferedImage(
@@ -80,16 +85,19 @@ public class CharacterSelectScreen implements Screen {
 //        bi = ImageIO.read(new File("items/default_shield.png"));
 //        g.drawImage(bi, 17, 320, null);
 
-        ImageIO.write(result,"png",new File("result.png"));
+        ImageIO.write(result, "png", new File("result.png"));
 
         return new Texture("result.png");
     }
 
     @Override
     public void show() {
+        stage.clear();
+        skin = new Skin(Gdx.files.internal("skin/glassy-ui.json"));
 
         float gameWidth = Gdx.graphics.getWidth();
         float gameHeight = Gdx.graphics.getHeight();
+        float pad = 5;
 
         // add the character image
 //        Texture texture = new Texture("default.png");
@@ -102,15 +110,14 @@ public class CharacterSelectScreen implements Screen {
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        if (texture == null){
+        if (texture == null) {
             texture = new Texture("default.png");
         }
         Image image = new Image(texture);
 
         // remove and add buttons
         TextButton create = new TextButton("Create new +", skin, "square");
-        TextButton remove1 = new TextButton("Logout", skin, "small");
-        TextButton remove2 = new TextButton("Back", skin, "small");
+        TextButton logout = new TextButton("Logout", skin, "small");
 
         // add the list of already created characters
         ArrayList<String> strings = new ArrayList<String>();
@@ -118,40 +125,59 @@ public class CharacterSelectScreen implements Screen {
         if (characterNames != null)
             strings = PlayerAccount.getCharactersName();
         else
-            for (int i = 0, k = 0; i < 10; i++) {
-                strings.add("String: " + i);
-            }
+            strings.add("No Characters");
 
         Table list = new Table();
-        for (String elem : strings) {
-//            TextButton  selectCharacterButton;
-//
-//            selectCharacterButton = new TextButton(elem, skin, "square");
-//            selectCharacterButton.addListener(new SelectCharacter(elem));
-//            list.add(selectCharacterButton).fill();
 
-            list.add(new TextButton(elem, skin, "square")).fill();
-            list.add(new TextButton("X", skin, "square")).fill();
+        ArrayList<TextButton> characterNamesButtons = new ArrayList<TextButton>();
+        ArrayList<TextButton> xButtons = new ArrayList<TextButton>();
+
+        for (int i = 0; i < strings.size(); i++) {
+            characterNamesButtons.add(new TextButton(strings.get(i), skin, "square"));
+            xButtons.add(new TextButton("X", skin, "square"));
+
+            list.add(characterNamesButtons.get(i)).fillX().expandX();
+            list.add(xButtons.get(i)).fillX();
             list.row();
 
+            characterNamesButtons.get(i).addListener(new ChangeListener() {
+                @Override
+                public void changed(ChangeEvent event, Actor actor) {
+                    System.out.println("Selected character " + actor.getName());
+                }
+            });
+
+            xButtons.get(i).addListener(new ChangeListener() {
+                @Override
+                public void changed(ChangeEvent event, Actor actor) {
+                    System.out.println("Deleted character " + actor.getName());
+                }
+            });
         }
-        list.add(create).colspan(2).fill();
-//        list = new List<String>(skin);
-//        list.setItems(strings.toArray(new String[strings.size()]));
+        list.add(create).fill().uniformY().colspan(2);
 
         scrollPane = new ScrollPane(list);
         scrollPane.setSmoothScrolling(false);
         scrollPane.setScrollingDisabled(true, false);
+        scrollPane.setScrollbarsOnTop(true);
+
+        // add the list and buttons table
+        Table buttonTable = new Table();
+        buttonTable.add(logout).fill().pad(0, 0, pad / 2, 0);
+        buttonTable.row();
+        buttonTable.add(scrollPane).fillX().expand().top().pad(pad / 2, 0, 0, 0);
+
+        // add wrapper table
+        Table screen = new Table();
+        screen.setFillParent(true);
+        screen.add(image).fill().uniform().pad(pad, pad, pad, pad / 2);
+        screen.add(buttonTable).fill().uniform().pad(pad, pad / 2, pad, pad);
+
+        stage.addActor(screen);
+        stage.setDebugAll(true);
 
         // add event listeners
-        remove1.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                PlayerAccount.logOut();
-                parent.changeScreen(parent.getLogin());
-            }
-        });
-        remove2.addListener(new ChangeListener() {
+        logout.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
                 PlayerAccount.logOut();
@@ -162,39 +188,19 @@ public class CharacterSelectScreen implements Screen {
         create.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
-                //parent.changeScreen(parent.getMenu());
+                //parent.changeScreen(parent.getMenu()); //go to create character screen
             }
         });
-
-        list.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-            }
-        });
-
-        // add the list and buttons table
-        Table buttonTable = new Table();
-        buttonTable.add(remove1);
-        buttonTable.add(remove2);
-        buttonTable.row();
-        buttonTable.add(scrollPane).colspan(2).width(gameWidth/2.5f).padLeft(10).padRight(10);
-
-        // add wrapper table
-        Table screen = new Table();
-        screen.setFillParent(true);
-        screen.add(image).width(gameHeight * 0.95f).height(gameHeight * 0.95f).expand();
-        screen.add(buttonTable).expand().padBottom(10).padTop(10);
-
-        stage.addActor(screen);
-        //stage.setDebugAll(true);
 
         Gdx.input.setInputProcessor(stage);
     }
 
     @Override
     public void render(float delta) {
+        //camera.update();
         Gdx.gl.glClearColor(0f, 0f, 0f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
         // tell our stage to do actions and draw itself
         stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
         stage.draw();
@@ -224,7 +230,7 @@ public class CharacterSelectScreen implements Screen {
 
 
     class SelectCharacter extends ChangeListener {
-        String  name;
+        String name;
 
         public SelectCharacter(String elem) {
             this.name = elem;
