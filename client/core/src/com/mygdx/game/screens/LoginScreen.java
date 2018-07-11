22d2ddbafb40d49126c58271ca017d1b9353eb44
apapuc97 <apapuc97@gmail.com>
2018-07-11 09:42:38 +0300

    Create character functionality update

--- client/core/src/com/mygdx/game/screens/LoginScreen.java
+++ client/core/src/com/mygdx/game/screens/LoginScreen.java
@@ -16,35 +16,40 @@ import com.mygdx.game.requests.Player;
 import com.mygdx.game.requests.PlayerAccount;
 import com.mygdx.game.gameSets.GGame;
 
-public class LoginScreen implements Screen{
-    // New version
+import de.tomgrill.gdxdialogs.core.GDXDialogs;
+import de.tomgrill.gdxdialogs.core.GDXDialogsSystem;
+import de.tomgrill.gdxdialogs.core.dialogs.GDXButtonDialog;
+import de.tomgrill.gdxdialogs.core.listener.ButtonClickListener;
 
-    private GGame parent;
+public class LoginScreen implements Screen{
+	// New version
+
+	private GGame parent;
 	private Stage stage;
 	private Skin skin;
 	private Label label;
 	private Label labelName;
 	private Label labelPassword;
-
-
+	private TextButton forgotPassword;
 	private Viewport viewport;
 	private Camera camera;
 	private Music loginMusic;
+	private GDXDialogs dialogs;
 
 
 
-    public LoginScreen(GGame g){
-    	parent = g;
-
+	public LoginScreen(GGame g){
+		parent = g;
+		dialogs = GDXDialogsSystem.install();
 		stage = new Stage();
 		viewport = new StretchViewport(800, 480, stage.getCamera());
 
 		stage.setViewport(viewport);
-        }
+	}
 
-    @Override
-        public void show() {
-    	stage.clear();
+	@Override
+	public void show() {
+		stage.clear();
 
 		skin = new Skin(Gdx.files.internal("skin/glassy-ui.json"));
 
@@ -72,6 +77,8 @@ public class LoginScreen implements Screen{
 		passwordField.setPasswordMode(true);
 		passwordField.setMessageText("Password goes here");
 
+		//Forgot password
+		forgotPassword = new TextButton("Forgot password?", skin, "small");
 		//add buttons to table
 		TextButton register = new TextButton("Register", skin);
 		TextButton submit = new TextButton("Submit", skin);
@@ -85,51 +92,53 @@ public class LoginScreen implements Screen{
 		});
 
 
+
 		//add listeners to buttons
 		submit.addListener(new SubmitListener(loginField, passwordField));
-		/*
-		submit.addListener(new ChangeListener() {
-			@Override
-			public void changed(ChangeEvent event, Actor actor) {
-				Player player = null;
+		forgotPassword.addListener(new ForgotPassword());
+  /*
+  submit.addListener(new ChangeListener() {
+   @Override
+   public void changed(ChangeEvent event, Actor actor) {
+    Player player = null;
 
-				if (loginField.getText() == "")
-					label.setText("Write the login please");
-				else if (passwordField.getText() == "")
-					label.setText("Write the password please");
-				else {
-					String	encryptedPassword;
+    if (loginField.getText() == "")
+     label.setText("Write the login please");
+    else if (passwordField.getText() == "")
+     label.setText("Write the password please");
+    else {
+     String encryptedPassword;
 
-					//encryptedPassword = EncryptPassword.encrypt(passwordField.getText());
-					encryptedPassword = passwordField.getText();
-					try {
-						try {
-							player = Player.loginPlayer(loginField.getText(), encryptedPassword);
-						} catch (JSONException e) {
-							e.printStackTrace();
-						}
-						encryptedPassword = "";
-						if (player == null) {
-							if (JsonHandler.errorMessage != null)
-								label.setText(JsonHandler.errorMessage);
-							else
-								label.setText("Something went wrong");
-						}
-						else {
-							passwordField.setText("");
-							PlayerAccount.setPlayer(player);
-							parent.changeScreen(parent.getCharacterSelect());
-						}
-					} catch (IOException e) {
-						e.printStackTrace();
-						if (JsonHandler.errorMessage != null)
-							label.setText(JsonHandler.errorMessage);
-						else
-							label.setText("Something went wrong");
-					}
-				}
-			}
-		});*/
+     //encryptedPassword = EncryptPassword.encrypt(passwordField.getText());
+     encryptedPassword = passwordField.getText();
+     try {
+      try {
+       player = Player.loginPlayer(loginField.getText(), encryptedPassword);
+      } catch (JSONException e) {
+       e.printStackTrace();
+      }
+      encryptedPassword = "";
+      if (player == null) {
+       if (JsonHandler.errorMessage != null)
+        label.setText(JsonHandler.errorMessage);
+       else
+        label.setText("Something went wrong");
+      }
+      else {
+       passwordField.setText("");
+       PlayerAccount.setPlayer(player);
+       parent.changeScreen(parent.getCharacterSelect());
+      }
+     } catch (IOException e) {
+      e.printStackTrace();
+      if (JsonHandler.errorMessage != null)
+       label.setText(JsonHandler.errorMessage);
+      else
+       label.setText("Something went wrong");
+     }
+    }
+   }
+  });*/
 
 		settings.addListener(new ChangeListener() {
 			@Override
@@ -149,6 +158,8 @@ public class LoginScreen implements Screen{
 		table.row().pad(40, 10, 0, 10);
 		table.add(register);
 		table.add(submit);
+		table.row().pad(20, 0, 0, 0);
+		table.add(forgotPassword);
 
 
 
@@ -160,8 +171,8 @@ public class LoginScreen implements Screen{
 	}
 
 	@Override
-        public void render(float delta) {
-    	//camera.update();
+	public void render(float delta) {
+		//camera.update();
 		Gdx.gl.glClearColor(0f, 0f, 0f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
@@ -171,32 +182,34 @@ public class LoginScreen implements Screen{
 	}
 
 	@Override
-        public void resize(int width, int height) {
+	public void resize(int width, int height) {
 
 
-    	stage.getViewport().update(width,height,true);
+		stage.getViewport().update(width,height,true);
 	}
- 
+
 	@Override
-        public void pause() {
+	public void pause() {
 	}
- 
+
 	@Override
-        public void resume() {
+	public void resume() {
 	}
- 
+
 	@Override
-        public void hide() {
+	public void hide() {
 	}
- 
+
 	@Override
-        public void dispose() {
-			stage.dispose();
+	public void dispose() {
+		stage.dispose();
 	}
+
+
 
 	class SubmitListener extends ChangeListener {
-    	private TextField	loginField;
-    	private TextField	passwordField;
+		private TextField loginField;
+		private TextField passwordField;
 
 		public SubmitListener(TextField loginField, TextField passwordField) {
 			this.loginField = loginField;
@@ -212,18 +225,18 @@ public class LoginScreen implements Screen{
 			else if (passwordField.getText() == "")
 				label.setText("Write the password please");
 			else {
-				String	encryptedPassword;
+				String encryptedPassword;
 
 				encryptedPassword = EncryptPassword.encrypt(passwordField.getText());
 				//encryptedPassword = passwordField.getText();
 				try {
 					if (PlayerAccount.loginPlayer(loginField.getText(), encryptedPassword)) {
-							parent.changeScreen(parent.getCharacterSelect());
+						parent.changeScreen(parent.getCharacterSelect());
 					}
 					else {
-							label.setText(JsonHandler.errorMessage);
+						label.setText(JsonHandler.errorMessage);
 					}
-						//player = Player.loginPlayer(loginField.getText(), encryptedPassword);
+					//player = Player.loginPlayer(loginField.getText(), encryptedPassword);
 				} catch (Exception e) {
 					e.printStackTrace();
 					if (JsonHandler.errorMessage != null)
@@ -236,156 +249,19 @@ public class LoginScreen implements Screen{
 		}
 	}
 
+	class ForgotPassword extends ChangeListener{
+
+		@Override
+		public void changed(ChangeEvent changeEvent, Actor actor) {
+			forgotPassword();
+		}
+	}
+
+	private void forgotPassword(){
+		final GDXButtonDialog bDialog = dialogs.newDialog(GDXButtonDialog.class);
+		bDialog.setTitle("Lol");
+		bDialog.setMessage("Too bad");
+		bDialog.addButton("Go back");
+		bDialog.build().show();
+	}
 }
-    
-
-/*
-
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input.TextInputListener;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.scenes.scene2d.ui.TextField;
-import com.badlogic.gdx.utils.viewport.StretchViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
-
-public class LoginScreen implements Screen {
-
-    final GGame game;
-    OrthographicCamera camera;
-
-    private Stage stage;
-    private TextField.TextFieldStyle textFieldStyle;
-    private float densityT = 60 * Gdx.graphics.getDensity();
-    private BitmapFont bitmapFont;
-    
-    private    Table tableMain, tableMenu;
-    private    Label title;
-    private    TextButton button;
-    private    TextButton.TextButtonStyle buttonStyle, buttonStyleAds;
-
-    public LoginScreen(final GGame gam) {
-        game = gam;
-
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false, 800, 480);
-    }
-
-    @Override
-    public void render(float delta) {
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        camera.update();
-        game.batch.setProjectionMatrix(camera.combined);
-
-        game.batch.begin();
-        game.batch.end();
-
-        if (Gdx.input.isTouched()) {
-            game.setScreen(new GameScreen(game));
-            dispose();
-        }
-    }
-
-    class TextInput implements TextInputListener {
-
-        @Override
-        public void input(String text) {
-        }
-
-        @Override
-        public void canceled() {
-        }
-    }
-
-    @Override
-    public void resize(int width, int height) {
-    }
-
-    @Override
-    public void show() {
-        TextInput listener = new TextInput();
-        Gdx.input.getTextInput(listener, "MonkaS", "Type here", "alo");
-
-        stage = new Stage();
-        Viewport vp = new StretchViewport(800, 480, camera);
-        stage = new Stage(vp);
-        Gdx.input.setInputProcessor(stage);
-        ButtonClickListener buttonClickListener = new ButtonClickListener();
-        SceneLoader sc = new SceneLoader();
-        CompositeItemVO sceneComposites = new CompositeItemVO(sc.loadScene("Login").composite);
-        CompositeActor UI = new CompositeActor(sceneComposites, sc.getRm());
-        stage.addActor(UI);
-        UI.getItem("regisbutton").addListener(buttonClickListener);
-        UI.getItem("loginbutton").addListener(buttonClickListener);
-        textFieldStyle = new TextField.TextFieldStyle();
-        textFieldStyle.font = bitmapFont;
-        textFieldStyle.fontColor = Color.WHITE;
-        TextField username = new TextField("Username", textFieldStyle);
-        TextField password = new TextField("Password", textFieldStyle);
-        password.setPosition(400, 400);
-        username.setPosition(400, 500);
-        stage.addActor(username);
-        stage.addActor(password);
-    }
-
-    @Override
-    public void hide() {
-    }
-
-    @Override
-    public void pause() {
-    }
-
-    @Override
-    public void resume() {
-    }
-
-    @Override
-    public void dispose() {
-    }
-    
-    class ButtonClickListener implements {
-        
-    }
-    
-    
-    public class SimpleActor extends Actor {
-
-    private final TextureRegion region;
-
-    public SimpleActor(TextureRegion region) {
-        this.region = region;
-        setSize(region.getRegionWidth(), region.getRegionHeight());
-        setBounds(0, 0, getWidth(), getHeight());
-    }
-
-    @Override
-    public void draw(Batch batch, float parentAlpha) {
-        batch.draw(region, getX(), getY(), getOriginX(), getOriginY(), getWidth(), getHeight(), getScaleX(), getScaleY(), getRotation());
-    }
-}
-    
-}
-/*
-    @Override
-    public void input(String text) {
-    }
-
-    @Override
-    public void canceled() {
-    }
- */
