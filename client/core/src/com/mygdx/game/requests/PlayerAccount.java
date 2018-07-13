7643a39729d9f956b7a9a7472a0190289be3cb5b
iiasceri <34134411+iiasceri@users.noreply.github.com>
2018-07-13 15:26:36 +0300

    Team members wins and loss

--- client/core/src/com/mygdx/game/requests/PlayerAccount.java
+++ client/core/src/com/mygdx/game/requests/PlayerAccount.java
@@ -28,6 +28,18 @@ public class PlayerAccount {
         return player.getCharactersName();
     }
 
+    public static Integer getLosses() {
+        if (team == null)
+            return null;
+        return team.getTeamLoss();
+    }
+
+    public static Integer getWins() {
+        if (team == null)
+            return null;
+        return team.getTeamWins();
+    }
+
     public static String getTeamName(){
         if(team == null)
             return null;
