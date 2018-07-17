a3b4708967a7a7a8babc5aea1be5cbcaa9825862
iiasceri <34134411+iiasceri@users.noreply.github.com>
2018-07-17 16:29:58 +0300

    Settings pop-up is done!

--- client/core/src/com/mygdx/game/screens/LoginScreen.java
+++ client/core/src/com/mygdx/game/screens/LoginScreen.java
@@ -8,6 +8,8 @@ import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Event;
+import com.badlogic.gdx.scenes.scene2d.EventListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
@@ -40,6 +42,14 @@ public class LoginScreen implements Screen{
 	private Music loginMusic;
 	private GDXDialogs dialogs;
 	private BackgroundAnimation animationScreenTest;
+
+	//trying...
+    private Label titleLabel;
+    private Label volumeMusicLabel;
+    private Label volumeSoundLabel;
+    private Label musicOnOffLabel;
+    private Label soundOnOffLabel;
+    //trying...
 
 	private Sound click;
 	private Skin skin2;
@@ -75,6 +85,77 @@ public class LoginScreen implements Screen{
 //		table.setDebug(true);
 
 		stage.addActor(table);
+
+		/*Settings! trying...*/
+
+
+
+        //music volume
+        final Slider volumeMusicSlider = new Slider( 0f, 1f, 0.1f,false, skin );
+        volumeMusicSlider.setValue(parent.getPreferences().getMusicVolume());
+        volumeMusicSlider.addListener( new EventListener() {
+            @Override
+            public boolean handle(Event event) {
+                parent.getPreferences().setMusicVolume( volumeMusicSlider.getValue() );
+                return false;
+            }
+        });
+        //sound volume
+        final Slider volumeSoundSlider = new Slider( 0f, 1f, 0.1f,false, skin );
+        volumeSoundSlider.setValue( parent.getPreferences().getSoundVolume());
+        volumeSoundSlider.addListener( new EventListener() {
+            @Override
+            public boolean handle(Event event) {
+                parent.getPreferences().setSoundVolume(volumeSoundSlider.getValue());
+                return false;
+            }
+        });
+
+
+
+        //music
+        final CheckBox musicCheckbox = new CheckBox(null, skin);
+        musicCheckbox.setChecked( parent.getPreferences().isMusicEnabled() );
+        musicCheckbox.addListener( new EventListener() {
+            @Override
+            public boolean handle(Event event) {
+                boolean enabled = musicCheckbox.isChecked();
+                parent.getPreferences().setMusicEnabled( enabled );
+                return false;
+            }
+        });
+        //sound
+        final CheckBox soundCheckbox = new CheckBox(null, skin );
+        soundCheckbox.setChecked( parent.getPreferences().isSoundEnabled() );
+        soundCheckbox.addListener( new EventListener() {
+            @Override
+            public boolean handle(Event event) {
+                boolean enabled = soundCheckbox.isChecked();
+                parent.getPreferences().setSoundEnabled( enabled );
+                return false;
+            }
+        });
+
+        //return to main screen
+        final TextButton back = new TextButton("Back", skin);
+        back.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                parent.changeScreen(parent.getBackFromSettings());
+            }
+        });
+
+        //making labels
+        titleLabel = new Label( "Preferences", skin);
+        volumeMusicLabel = new Label( "Music Volume", skin );
+        volumeSoundLabel = new Label( "Sound Volume", skin  );
+        musicOnOffLabel = new Label( "Music Effect", skin  );
+        soundOnOffLabel = new Label( "Sound Effect", skin  );
+
+        /*Settings! trying...*/
+
+
+
 
 
 		//add label
@@ -121,17 +202,27 @@ public class LoginScreen implements Screen{
 			@Override
 			public void changed(ChangeEvent event, Actor actor){
 				click.play();
+
                 Dialog dialog = new Dialog("Settings", skin) {
                     public void result(Object obj) {
                         System.out.println("result "+obj);
                     }
                 };
-                dialog.text("Are you sure you want to yada yada?");
-                dialog.button("Yes", true); //sends "true" as the result
-                dialog.button("No", false); //sends "false" as the result
+                dialog.getContentTable().row();
+                dialog.getContentTable().add(volumeMusicLabel);
+                dialog.getContentTable().add(volumeMusicSlider);
+                dialog.getContentTable().row();
+                dialog.getContentTable().add(musicOnOffLabel);
+                dialog.getContentTable().add(musicCheckbox);
+                dialog.getContentTable().row();
+                dialog.getContentTable().add(volumeSoundLabel);
+                dialog.getContentTable().add(volumeSoundSlider);
+                dialog.getContentTable().row();
+                dialog.getContentTable().add(soundOnOffLabel);
+                dialog.getContentTable().add(soundCheckbox);
+                dialog.getContentTable().row();
+                dialog.getContentTable().add(back).colspan(2);
                 dialog.show(stage);
-
-
 
                 //parent.changeScreen(parent.getSettings());
 			}
